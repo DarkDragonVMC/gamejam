85a8c0b8b26cfdd6a8c670070bc9405a063b537a
DarkDragonVMC <redfirestorm07@gmail.com>
2021-06-06 11:44:27 +0200

    test

---
+++ boardGame/Assets/Scripts/rotationWhenSpin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class rotationWhenSpin : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    
+    public void spin ()
+    {
+        rb.AddTorque(0, Random.Range(20, 300), 0);
+    }
+}
